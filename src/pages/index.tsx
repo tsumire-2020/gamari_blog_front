210c1c07791b6e5192ba9616b8808161ebfc7c7a
tumire <tumire.work.2020@gmail.com>
2023-08-02 23:14:21 +0900

    新規作成

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main 
+    className={
+`      flex justify-center 
+      items-center
+      h-screen`
+    }
+      >
+      center
+    </main>
+  )
+}
